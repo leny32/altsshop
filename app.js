8b01036830b11ee339a62bd42d1c5a781a596134
Glitch (altsshop) <none>
2020-05-25 19:35:13 +0000

    🐔🍢 Updated with Glitch

--- app.js
+++ app.js
@@ -1,36 +1,51 @@
 var express = require('express');
 var path = require('path');
+var ejs = require('ejs')
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://leny32:wGHL12lg1bIxombb@smartgiveaways-0guza.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(favicon(__dirname + '/public/images/nav.svg'));
+    app.use(logger('dev'));
+    app.use(methodOverride());
+    app.use(session({ resave: true, saveUninitialized: true, 
+                      secret: 'uwotm8' }));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // parse application/json
+    app.use(bodyParser.json());                        
+
+    // parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // parse multipart/form-data
+    app.use(multer());
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -42,5 +57,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
